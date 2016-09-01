ac389814ffc2292d6afac2d3b6dbaff6db15d16e
Hiroshi Saito <saidie@saidie.info>
2016-09-02 03:07:03 +0900

    Implement HelloTrieExist_RedisCommand

--- hello/trie.c
+++ hello/trie.c
@@ -203,6 +203,38 @@ int HelloTriePrettyPrint_RedisCommand(RedisModuleCtx *ctx, RedisModuleString **a
   return REDISMODULE_OK;
 }
 
+int HelloTrieExist_RedisCommand(RedisModuleCtx *ctx, RedisModuleString **argv, int argc) {
+  if (argc != 3) return RedisModule_WrongArity(ctx);
+
+  RedisModuleKey *key = RedisModule_OpenKey(ctx, argv[1],
+    REDISMODULE_READ | REDISMODULE_WRITE);
+
+  int type = RedisModule_KeyType(key);
+  if (type != REDISMODULE_KEYTYPE_EMPTY && RedisModule_ModuleTypeGetType(key) != TrieType)
+    return RedisModule_ReplyWithError(ctx, REDISMODULE_ERRORMSG_WRONGTYPE);
+
+  struct TrieTypeNode *n;
+  if (type == REDISMODULE_KEYTYPE_EMPTY) {
+    n = RedisModule_Calloc(1, sizeof(*n));
+    RedisModule_ModuleTypeSetValue(key, TrieType, n);
+  } else {
+    n = RedisModule_ModuleTypeGetValue(key);
+  }
+
+  size_t len;
+  const char *word = RedisModule_StringPtrLen(argv[2], &len);
+
+  int res = TrieTypeExist(n, word);
+
+  RedisModule_ReplyWithLongLong(ctx, res);
+
+  RedisModule_CloseKey(key);
+
+  RedisModule_ReplicateVerbatim(ctx);
+
+  return REDISMODULE_OK;
+}
+
 int RedisModule_OnLoad(RedisModuleCtx *ctx, RedisModuleString **argv, int argc) {
   if (RedisModule_Init(ctx, "hello", 1, REDISMODULE_APIVER_1) == REDISMODULE_ERR)
     return REDISMODULE_ERR;
